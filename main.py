d8a81e238cf0bebe34551ffb9c5f89d5104594b9
Sam Swanger <samuel.e.swanger@gmail.com>
2026-01-07 15:32:44 +0000

    Ui and stupid input changes still working on making ui look better

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import time #I got time so i can have "load time" so that way players have a buffer to read everything
+riddle_one = "To open the first lock solve this riddle (The answer is a whole number):\nI am the number of virtues that guide the soul,\nAnd the sins that take their toll.\nAnd the colors in a drop of morning dew.\nI am the sisters in the winter sky,\nAnd the pillars on which wisdom’s house does lie.\nI am the day the Creator finally chose to rest,\nThe perfect score, the ultimate test."
+
+riddle_two = ""
+riddle_three = ""
 
 class Puzzle():
     def __init__(self):
@@ -12,26 +16,30 @@ class Puzzle():
         print("Your username has been set!")
         make_linebreak()
         print("Now make sure to read the instructions carefully!")
+        time.sleep(2)
         print("The answer to a lock could be a word or number but it will be specified at the begining of the problem!")
-        print("If it is a number make it a whole number not the word (ex. 2 not two)")
+        time.sleep(2)
+        print("If the answer is a number make it a whole number not the word (ex. 2 not two)")
+        time.sleep(2)
         print("If you get stuck you can acsess a hint!")
+        time.sleep(2)
         print("Good luck!")
         print("Let's begin with the first lock!")
         make_linebreak()
         print("Loading...")
-        time.sleep(20.0)
+        time.sleep(3)
         self.present_lock_one()
     def present_lock_one(self):
         make_blankspace()
-        print("To open the first lock solve this riddle (The answer is a whole number):\nI am the number of virtues that guide the soul,\nAnd the sins that take their toll.\nAnd the colors in a drop of morning dew.\nI am the sisters in the winter sky,\nAnd the pillars on which wisdom’s house does lie.\nI am the day the Creator finally chose to rest,\nThe perfect score, the ultimate test.")
+        print(riddle_one)
         while True:
             dont_show = False
             make_linebreak()
             print("     1.|Hint")
             print("     2.|Answer")
+            print("     3.|Reset Puzzle")
             make_linebreak()
             x = int(input("     "))
-
             if x == 1:
                 print("This answer is greater than 5 but less than 12")
             elif x == 2:
@@ -44,6 +52,7 @@ class Puzzle():
                     print("you stupid dumb butt make it a whole number not a word")
                     dont_show = True
                 if x == 7:
+                    make_linebreak()
                     print("Congrats you passed the first challenge now onto the next puzzle!")
                     self.room_states["Room One"] = True
                     make_linebreak()
@@ -53,13 +62,17 @@ class Puzzle():
                     break
                 elif x != 7 and dont_show == False:
                     print("YOU ARE WRONG TRY AGAIN!!!")
+            elif x == 3:
+                print("Reseting...")
+                time.sleep(5.0)
+                self.present_lock_one()
+                break
             else:
                 print("Invalid Input")
     def present_lock_two(self):
         pass
     def present_lock_three(self):
         pass
-
 def make_blankspace(): #makes some blank room on the terminal so it looks nice
     for i in range(50):
         print()
@@ -68,5 +81,5 @@ def make_linebreak(): #It makes a very fun linebreak
 
 
 
-escapeRoom = Puzzle()
-escapeRoom.welcome_player()
+escape_room = Puzzle()
+escape_room.welcome_player()
